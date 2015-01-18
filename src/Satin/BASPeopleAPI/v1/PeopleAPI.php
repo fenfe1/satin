da87afb2f1d90d6e676ff36683132b5d0117d3a3
Felix Fennell <felix@felixfennell.co.uk>
2015-01-18 12:03:28 +0000

    Adding basic token methods
    Provides required methods to ensure a valid API token can be acquired or throw a fatal exception.
    
    This will be refactored into a BASAPI base class which the people API will then extend, since the token implementations will be the same across all the various APIs.

---
+++ src/Satin/BASPeopleAPI/v1/PeopleAPI.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Satin\BASPeopleAPI\v1;
+
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+class PeopleAPI {
+
+    private $scheme = 'http';
+
+    private $endpoint = 'api.bas.ac.uk';
+
+    private $api = 'people';
+
+    private $version = 1;
+
+    private $username;
+
+    private $password;
+
+    private $client;
+
+    private $token;
+
+    private $tokenExpiry;
+
+    function __construct($username, $password)
+    {
+        // Set credentials
+        $this->username = $username;
+        $this->password = $password;
+
+        // Create API client
+        $this->client = new Client([
+            'base_url' => [
+                '{scheme}://{endpoint}/{api}/v{version}/',
+                [
+                    'scheme' => $this->scheme,
+                    'endpoint' => $this->endpoint,
+                    'api' => $this->api,
+                    'version' => $this->version
+                ]
+            ],
+            'defaults' => [
+                'headers' => [
+                    'Accept' => 'application/json'
+                ]
+            ]
+        ]);
+    }
+
+    public function requestToken()
+    {
+        // TODO: handle 401 wrong credentials exception/error
+
+        try
+        {
+            $response = $this->client->post('tokens', [
+                'json' => [
+                    'username' => $this->username,
+                    'password' => $this->password
+                ]
+            ]);
+
+            // This check might be redundant
+            if ($response->getStatusCode() == 200)
+            {
+                $data = $response->json();
+
+                // Set token and token expiry
+                $this->token = $data['data']['token'];
+                $this->tokenExpiry = Carbon::createFromTimeStampUTC($data['notices'][0]['details']['expiry']['expires']);
+
+                // Persist token and token expiry for future requests
+                $this->storeToken();
+
+                return true;
+            }
+        }
+        catch (ClientException $e)
+        {
+            $response = $e->getResponse();
+
+            // Check if this was a credentials error
+            if ($response->getStatusCode(401))
+            {
+                $data = $response->json();
+                if ($data['errors'][0]['type'] == 'authentication_failure')
+                {
+                    // TODO: Throw invalid credentials exception
+                    echo "Invalid credentials exception!" . " \n";
+                    die();
+                }
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    private function storeToken()
+    {
+        // TODO: Store token and token expiry to file
+    }
+
+    private function retrieveToken()
+    {
+        // TODO: Retrieve token from file
+        // If token has expired return false even if token is set
+
+        return false;
+    }
+
+    private function getToken()
+    {
+        // Check if there's a valid class instance token we can use
+        if (isset($this->token) && isset($this->tokenExpiry))
+        {
+            return $this->checkTokenIsValid();
+        }
+
+        // Check if there's a valid persisted token we can use
+        $retrieveToken = $this->retrieveToken();
+        if ($retrieveToken === false)
+        {
+            return false;
+        }
+
+        return $this->checkTokenIsValid();
+    }
+
+    private function checkTokenIsValid()
+    {
+        // Ensure token hasn't already expired
+        if (Carbon::now()->gte($this->tokenExpiry))
+        {
+            return false;
+        }
+
+        return $this->token;
+    }
+
+    private function ensureValidToken()
+    {
+        // If a valid token already exists use it
+        if ($this->getToken() !== false)
+        {
+            return true;
+        }
+
+        // Request a new token
+        if ($this->requestToken() !== false)
+        {
+            return true;
+        }
+
+        // Unable to provide a valid token
+        // TODO: Raise no valid token exception
+        echo "Unable to get valid token exception!" . " \n";
+        die();
+    }
+
+    public function showToken()
+    {
+        $this->ensureValidToken();
+
+        return $this->token;
+    }
+
+}
--- src/satin.php
+++ src/satin.php
@@ -1,9 +1,22 @@
 <?php
 
+use Satin\BASPeopleAPI\v1\PeopleAPI;
+
 require '../vendor/autoload.php';  // Load Composer
 
-// TODO: Something useful
+// API credentials
+// TODO: Read from dot file for safety
+$apiUsername = 'basweb';
+$apiPassword = 'password';
+
+// Create instance of the API
+$peopleAPI = new PeopleAPI($apiUsername, $apiPassword);
 
 echo "\n";
 echo "Welcome" . " \n";
+
+echo "\n";
+echo "Fetching token ..." . " \n";
+echo $peopleAPI->showToken() . " \n";
+
 echo "\n";
